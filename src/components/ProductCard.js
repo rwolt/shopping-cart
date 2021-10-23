604f48e00dcbaa85469071225ce6ff67b6ae8bb7
Raymond Wolt <rjwolt@gmail.com>
2021-10-23 13:13:04 -0500

    Add product hover effect for store page

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,23 +3,23 @@ import { Link, useRouteMatch } from 'react-router-dom';
 import '../styles/ProductCard.css';
 
 const ProductCard = (props) => {
-
-    const button = <button id={props.id} onClick={props.handleAdd} className="add-to-cart-card">Add To Cart</button>
     const [hover, setHover] = useState('blur');
 
     const handleHoverFocus = (e) => {
         setHover('focus');
+        e.currentTarget.classList.add('focus');
     }
 
     const handleHoverBlur = (e) => {
         setHover('blur');
+        e.currentTarget.classList.remove('focus')
     }
 
     const match = useRouteMatch();
     return(
         <div className="product-card-container"
-             onMouseEnter={handleHoverFocus}
-             onMouseLeave={handleHoverBlur}>
+              onMouseEnter={handleHoverFocus}
+              onMouseLeave={handleHoverBlur}>
             <div className="product-product-box">
                 <Link to={`${match.url}/${props.id}`}>
                     <img className="product-card-image" src={props.image} alt={`${props.name} ${props.color}`}/>
@@ -28,10 +28,15 @@ const ProductCard = (props) => {
                     <p className="product-card-name">{props.name}  |  {props.size}</p>
                     <p className="product-card-color">{props.color}</p>
                     <p className="product-card-price">${props.price}.00</p>
-                    {hover==='focus' ? button : ''}
                 </div>
             </div>
-
+                <button
+                    id={props.id}
+                    onClick={props.handleAdd}
+                    // className={hover==='focus' ? "add-to-cart-card" : "add-to-cart-card hidden"}>
+                    className="add-to-cart-card hidden">
+                    Add To Cart
+                </button>
         </div>
     )
 }
